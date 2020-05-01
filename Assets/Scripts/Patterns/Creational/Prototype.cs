8db8435262b23eb0062a23b00436b6f1e3b25162
Fred <gocheckband@gmail.com>
2020-05-01 11:35:38 +0300

    prototype

---
+++ Assets/Scripts/Patterns/Creational/Prototype.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Patterns.Prototype
+{
+    public interface IPrototype
+    {
+        IPrototype Clone();
+    }
+
+    public class Enemy : IPrototype
+    {
+        public int x, y;
+
+        public Enemy(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Enemy(Enemy e)
+        {
+            this.x = e.x;
+            this.y = e.y;
+        }
+
+        public IPrototype Clone()
+        {
+            return new Enemy(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{x} + {y}";
+        }
+    }
+
+    public class SimpleEnemy : Enemy, IPrototype
+    {
+        public int z;
+
+        public SimpleEnemy(int x, int y, int z) 
+            : base(x, y)
+        {
+            this.z = z;
+        }
+
+        public SimpleEnemy(SimpleEnemy s)
+            : base(s.x, s.y)
+        {
+            this.z = z;
+        }
+
+        public SimpleEnemy Clone()
+        {
+            return new SimpleEnemy(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{x} + {y} + {z}";
+        }
+    }
+
+    public class Prototype : MonoBehaviour
+    {
+        public Button BuildButton;
+        public Text InfoText;
+        public Text CloneInfoText;
+        public Text CloneInfoText2;
+        public Text CloneInfoText3;
+        public Text CloneInfoText4;
+        public Text CloneInfoText5;
+
+        IPrototype e;
+        SimpleEnemy n;
+
+        private void OnEnable()
+        {
+            BuildButton.onClick.AddListener(Build);
+        }
+
+        private void Build()
+        {
+            e = new SimpleEnemy(3, 1, 2);
+            InfoText.text = e.ToString();
+
+            IPrototype clone = e.Clone();
+            CloneInfoText.text = clone.ToString();
+
+            Enemy clone2 = (Enemy)e.Clone();
+            CloneInfoText2.text = clone2.ToString();
+
+            IPrototype clone3 = (Enemy)e.Clone();
+            CloneInfoText3.text = clone3.ToString();
+
+            n = new SimpleEnemy(3, 1, 2);
+            CloneInfoText4.text = n.ToString();
+
+            SimpleEnemy clone4 = n.Clone();
+            CloneInfoText5.text = clone4.ToString();
+        }
+
+        private void OnDisable()
+        {
+            BuildButton.onClick.RemoveListener(Build);
+        }
+    }
+}
