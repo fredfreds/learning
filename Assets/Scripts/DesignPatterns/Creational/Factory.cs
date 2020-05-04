488c6a6fa19225e1310c10a529e27fcefa4aa2ad
Fred <gocheckband@gmail.com>
2020-05-04 22:58:15 +0300

    factory static

---
+++ Assets/Scripts/DesignPatterns/Creational/Factory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DesignPatterns.Factory
+{
+    public class Unit
+    {
+        private int heath, armor;
+        private Unit(int h, int a)
+        {
+            this.heath = h;
+            this.armor = a;
+        }
+
+        public override string ToString()
+        {
+            return $"{heath}/{armor}";
+        }
+
+        public static class UnitFactory
+        {
+            public static Unit CreateUnit(int h, int a)
+            {
+                return new Unit(h, a);
+            }
+        }
+    }
+
+    public class Factory : MonoBehaviour
+    {
+        public Button BuildButton;
+        public Text InfoText;
+
+        private void OnEnable()
+        {
+            BuildButton.onClick.AddListener(Build);
+        }
+
+        private void Build()
+        {
+            InfoText.text = Unit.UnitFactory.CreateUnit(10, 5).ToString();
+        }
+
+        private void OnDisable()
+        {
+            BuildButton.onClick.RemoveListener(Build);
+        }
+    }
+}
