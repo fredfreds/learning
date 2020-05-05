cdf1bf4f3f77be4b251bb70a8d49a1e8c061f705
Fred <gocheckband@gmail.com>
2020-05-05 20:41:16 +0300

    adapter

---
+++ Assets/Scripts/DesignPatterns/Structural/Adapter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DesignPatterns.Adapter
+{
+    public class Unit
+    {
+        public int Health;
+    }
+
+    public interface IUnit
+    {
+        int Health { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int GetHealth(this IUnit u)
+        {
+            return u.Health * 10 / 100;
+        }
+    }
+
+    public class UnitToIUnitAdapter : IUnit
+    {
+        private readonly Unit unit;
+
+        public UnitToIUnitAdapter(Unit u)
+        {
+            this.unit = u;
+        }
+
+        public int Health => unit.Health;
+    }
+
+    public class Adapter : MonoBehaviour
+    {
+        public Button BuildButton;
+
+        public Text InfoText;
+
+        Unit u;
+        UnitToIUnitAdapter a;
+
+        private void OnEnable()
+        {
+            BuildButton.onClick.AddListener(Build);
+        }
+
+        private void Build()
+        {
+            u = new Unit();
+            u.Health = 100;
+
+            a = new UnitToIUnitAdapter(u);
+            InfoText.text = u.Health.ToString() + "/" + a.GetHealth().ToString(); 
+        }
+
+        private void OnDisable()
+        {
+            BuildButton.onClick.RemoveListener(Build);
+        }
+    }
+}
