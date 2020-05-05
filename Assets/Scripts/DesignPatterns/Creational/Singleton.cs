322ad37e7b8596c61061051c3f73b2e71d69696d
Fred <gocheckband@gmail.com>
2020-05-05 18:20:01 +0300

    singleton lazy

---
+++ Assets/Scripts/DesignPatterns/Creational/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DesignPatterns.Singleton
+{
+    public interface IDatabase
+    {
+        string GetName();
+    }
+    public class Database : IDatabase
+    {
+        private string Name;
+
+        private static int count;
+
+        private Database()
+        {
+            count++;
+            Name = "Fred";
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public static int Count()
+        {
+            return count;
+        }
+
+        private static Lazy<Database> instance = new Lazy<Database>(() => new Database());
+        public static Database Instance => instance.Value;
+    }
+
+    public class Singleton : MonoBehaviour
+    {
+        public Button BuildButton;
+        public Text InfoText;
+
+        private void OnEnable()
+        {
+            BuildButton.onClick.AddListener(Build);
+        }
+
+        private void Build()
+        {
+            IDatabase db = Database.Instance;
+            InfoText.text = db.GetName();
+        }
+
+        private void OnDisable()
+        {
+            BuildButton.onClick.RemoveListener(Build);
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/SingletonTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DesignPatterns.Singleton;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SingletonTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void IsDatabaseSingleton()
+        {
+            Database db = Database.Instance;
+            Database db1 = Database.Instance;
+            Assert.That(db, Is.SameAs(db1));
+            Assert.That(Database.Count, Is.EqualTo(1));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SingletonTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
