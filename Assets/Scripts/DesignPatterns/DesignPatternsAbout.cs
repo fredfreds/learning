67224b03c926419d37d81073868b353b8624af2e
Fred <gocheckband@gmail.com>
2020-05-10 02:42:02 +0300

    design patterns information

---
+++ Assets/Scripts/DesignPatterns/DesignPatternsAbout.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DesignPatterns.DesignPatternsAbout
+{
+    public class DesignPatternsAbout
+    {
+        //  CREATIONAL:
+        //-------------------------------------------------------
+        // 1. Builder:
+        // - separate component for object construction
+        // - fluent interface
+        //-------------------------------------------------------
+        // 2-3. Factories: Factory and Abstract Factory
+        // - accessing private members
+        // - more expressive than constructor
+        //-------------------------------------------------------
+        // 4. Prototype:
+        // - creation from existing
+        // - deep copy / copy through serialization
+        //-------------------------------------------------------
+        // 5. Singleton:
+        // - single instance, thread-safe and lazy
+        // - extracting interface or DI
+        //-------------------------------------------------------
+        /////////////////////////////////////////////////////////
+        //-------------------------------------------------------
+        //  STRUCTURAL:
+        //-------------------------------------------------------
+        // 1(6). Adapter:
+        // - convert to needed interface 
+        // - 
+        //-------------------------------------------------------
+        // 2(7). Bridge:
+        // - decouple abstraction from implementation
+        // - 
+        //-------------------------------------------------------
+        // 3(8). Composite:
+        // - 
+        // - 
+        //-------------------------------------------------------
+        // 4(9). Decorator:
+        // - add additional responsibilities
+        // - 
+        //-------------------------------------------------------
+        // 5(10). Facade:
+        // - single unified interface
+        // - 
+        //-------------------------------------------------------
+        // 6(11). Flyweight:
+        // - cache of similar large objects
+        // - 
+        //-------------------------------------------------------
+        // 7(12). Proxy:
+        // - surrogate object
+        // - 
+        //-------------------------------------------------------
+        /////////////////////////////////////////////////////////
+        //-------------------------------------------------------
+        //  BEHAVIORAL:
+        //-------------------------------------------------------
+        // 1(13). Chain of Responsibility:
+        // - information/events in a chain
+        // - each element in the chain refers to next element
+        //-------------------------------------------------------
+        // 2(14). Command:
+        // - undo/redo
+        // - encapsulate into a separate object
+        //-------------------------------------------------------
+        // 3(15). Interpreter:
+        // - interpreters/compilers
+        // - 
+        //-------------------------------------------------------
+        // 4(16). Iterator:
+        // - IEnumerable<T>
+        // - 
+        //-------------------------------------------------------
+        // 5(17). Mediator:
+        // - chat
+        // - 
+        //-------------------------------------------------------
+        // 6(18). Memento:
+        // - tokens for system states
+        // - 
+        //-------------------------------------------------------
+        // 7(19). Observer:
+        // - event
+        // - 
+        //-------------------------------------------------------
+        // 8(20). State:
+        // - state machine
+        // - 
+        //-------------------------------------------------------
+        // 9(21). Strategy:
+        // - composition (interfaces)
+        // - 
+        //-------------------------------------------------------
+        // 10(22). Template:
+        // - inheritance (abstract classes)
+        // - 
+        //-------------------------------------------------------
+        // 11(23). Visitor:
+        // - double dispatch
+        // - 
+        //-------------------------------------------------------
+    }
+}
