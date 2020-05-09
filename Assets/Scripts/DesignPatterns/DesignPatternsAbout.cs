d8a279a4bd5c9a160963651bb33523a9b91d9a15
Fred <gocheckband@gmail.com>
2020-05-10 02:54:24 +0300

    add links to patterns

--- Assets/Scripts/DesignPatterns/DesignPatternsAbout.cs
+++ Assets/Scripts/DesignPatterns/DesignPatternsAbout.cs
@@ -12,18 +12,23 @@ namespace DesignPatterns.DesignPatternsAbout
         // 1. Builder:
         // - separate component for object construction
         // - fluent interface
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Creational/Builder.cs
         //-------------------------------------------------------
         // 2-3. Factories: Factory and Abstract Factory
         // - accessing private members
         // - more expressive than constructor
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Creational/Factory.cs
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Creational/AbstractFactory.cs
         //-------------------------------------------------------
         // 4. Prototype:
         // - creation from existing
         // - deep copy / copy through serialization
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Creational/Prototype.cs
         //-------------------------------------------------------
         // 5. Singleton:
         // - single instance, thread-safe and lazy
         // - extracting interface or DI
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Creational/Singleton.cs
         //-------------------------------------------------------
         /////////////////////////////////////////////////////////
         //-------------------------------------------------------
@@ -32,30 +37,37 @@ namespace DesignPatterns.DesignPatternsAbout
         // 1(6). Adapter:
         // - convert to needed interface 
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Adapter.cs
         //-------------------------------------------------------
         // 2(7). Bridge:
         // - decouple abstraction from implementation
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Bridge.cs
         //-------------------------------------------------------
         // 3(8). Composite:
         // - 
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Composite.cs
         //-------------------------------------------------------
         // 4(9). Decorator:
         // - add additional responsibilities
-        // - 
+        // -
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Decorator.cs
         //-------------------------------------------------------
         // 5(10). Facade:
         // - single unified interface
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Facade.cs
         //-------------------------------------------------------
         // 6(11). Flyweight:
         // - cache of similar large objects
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Flyweight.cs
         //-------------------------------------------------------
         // 7(12). Proxy:
         // - surrogate object
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Structural/Proxy.cs
         //-------------------------------------------------------
         /////////////////////////////////////////////////////////
         //-------------------------------------------------------
@@ -64,46 +76,61 @@ namespace DesignPatterns.DesignPatternsAbout
         // 1(13). Chain of Responsibility:
         // - information/events in a chain
         // - each element in the chain refers to next element
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/ChainOfResponsibility.cs
         //-------------------------------------------------------
         // 2(14). Command:
         // - undo/redo
         // - encapsulate into a separate object
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Command.cs
         //-------------------------------------------------------
         // 3(15). Interpreter:
         // - interpreters/compilers
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Interpreter.cs
         //-------------------------------------------------------
         // 4(16). Iterator:
         // - IEnumerable<T>
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Iterator.cs
         //-------------------------------------------------------
         // 5(17). Mediator:
         // - chat
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Mediator.cs
         //-------------------------------------------------------
         // 6(18). Memento:
         // - tokens for system states
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Memento.cs
         //-------------------------------------------------------
         // 7(19). Observer:
         // - event
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Observer.cs
         //-------------------------------------------------------
         // 8(20). State:
         // - state machine
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/State.cs
         //-------------------------------------------------------
         // 9(21). Strategy:
         // - composition (interfaces)
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Strategy.cs
         //-------------------------------------------------------
         // 10(22). Template:
         // - inheritance (abstract classes)
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Template.cs
         //-------------------------------------------------------
         // 11(23). Visitor:
         // - double dispatch
         // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/Visitor.cs
         //-------------------------------------------------------
+        // 12. Null Object:
+        // - 
+        // - 
+        // - https://github.com/fredfreds/learning/blob/master/Assets/Scripts/DesignPatterns/Behavioral/NullObject.cs
     }
 }
