49f95afd1b8e2a5470ddab3e3e7d9de6fd06d789
Fred <gocheckband@gmail.com>
2020-06-23 19:37:55 +0300

    c++ plug-in

---
+++ Assets/Other/Plug-ins/CPlusPlusPluginTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Plugins
+{
+    public class CPlusPlusPluginTest : UnityEngine.MonoBehaviour
+    {
+        const string dll = "Dll2";
+
+        [DllImport(dll)]
+        private static extern float Add(float a, float b);
+
+        [DllImport(dll)]
+        private static extern IntPtr Hello();
+
+        private void Start()
+        {
+            UnityEngine.Debug.Log(Add(3, 4));
+            UnityEngine.Debug.Log(Marshal.PtrToStringAnsi(Hello()));
+        }
+    }
+}
